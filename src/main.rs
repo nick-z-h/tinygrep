1c60d48897454fdfc599e4fa537984a8ca367e8e
n <n.zhanghoule@gmail.com>
2022-02-26 11:22:51 -0500

    .

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use std::{env, process};
+use tinygrep::Config;
+
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arugments: {}", err);
+        process::exit(1);
+    });
+    if let Err(e) = tinygrep::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
